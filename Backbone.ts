61dff72a2c17aa0e816bea1e183f0ab4aaf098ba
jbaldwin <joshrbaldwin@gmail.com>
2012-12-26 13:07:32 -0700

    Added libraries
    jQuery
    Underscore

--- Backbone.ts
+++ Backbone.ts
@@ -1,4 +1,12 @@
+/// <reference path="lib/underscore-1.4.d.ts" />
+/// <reference path="lib/jQuery-1.8.d.ts" />
 
 module Backbone {
+
+	export var Version = "0.1";
+
+	export var $ = jQuery;
+
+	export var _ = _;
 
 }
